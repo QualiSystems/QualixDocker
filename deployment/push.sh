0e096d8505348808f6931aa0b95fe76a66ab83d5
Moshe Reubinoff <reubinoff@gmail.com>
2019-06-04 09:07:42 +0300

    mosify push

--- deployment/push.sh
+++ deployment/push.sh
@@ -2,6 +2,7 @@
 
 echoerr()( echo $@|sed $'s,.*,\e[31m&\e[m,'>&2)3>&1
 echok()( echo $@|sed $'s,.*,\e[32m&\e[m,'>&2)3>&1
+echo()( echo $@|sed $'s,.*,\e[33m&\e[m,'>&2)3>&1
 
 function setError {
     echoerr "Pushing $1 image Failed! please check $1 log"
@@ -23,28 +24,34 @@ function push {
 BASE_REPO=quali
 QUACD=qualix_guacd
 GIACAMOLE=qualix_guacamole
-VERSION=1.0.0
-TAG=moshe_tag
 NAME=moshe_name
-REPO=qualitesthub
+
+if [ -z "$VERSION" ]
+then
+    VERSION=1.0.0
+fi
+
+if [ -z "$DOCKER_USERNAME" ]
+then
+    echo "No Docker hub username detected"
+    DOCKER_USERNAME=YourUserName
+fi
+if [ -z "$DOCKER_PASSWORD" ]
+then
+    echo "No Docker hub password detected"
+    DOCKER_PASSWORD=YouPassword
+fi
+
 
 # Tagging the images
 QUACD_TAG=$REPO/$QUACD:$VERSION
 GIACAMOLE_TAG=$REPO/$GIACAMOLE:$VERSION
 
-docker tag $BASE_REPO/$QUACD:latest $REPO/$QUACD:$VERSION
-docker tag $BASE_REPO/$GIACAMOLE:latest $REPO/$GIACAMOLE:$VERSION
+docker tag $BASE_REPO/$QUACD:latest $QUACD_TAG
+docker tag $BASE_REPO/$GIACAMOLE:latest $GIACAMOLE_TAG
 
 
 # Login to Docker
-if [ -z "$DOCKER_USERNAME" ]
-then
-    DOCKER_USERNAME=qualitesthub
-fi
-if [ -z "$DOCKER_PASSWORD" ]
-then
-    DOCKER_PASSWORD=12345678
-fi
 echo Login to docker hub with username: $DOCKER_USERNAME
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD > /dev/null 2>&1
 if [ $? -ne 0 ]; then
