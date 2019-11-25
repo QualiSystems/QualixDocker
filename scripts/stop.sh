04666f2005db3efe60bc15efd2a1198921760d4c
Meir.b <pelyem@gmail.com>
2019-11-25 15:39:22 +0200

    override docker-compose bug

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+#fix bug 173637 http://team:8080/tfs/Quali-Collection/QualiSystems/_workitems?id=173637&_a=edit
 cd /opt/qualix
+
+#this is done only to workaround this bug https://github.com/docker/compose/issues/6838
+mv '.env' '.env_old'
+
 docker-compose down
+
+#the rest of the workaround
+mv '.env_old' '.env'
 
 total_instances=$(docker ps | grep quali | wc -l)
 while [ "$total_instances" -ne "0" ]
