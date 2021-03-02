d5fda94047adf1d16ab6e58634b915a132197f47
Yaroslav Nikonorov <yaroslav.n@quali.com>
2021-03-02 19:06:02 +0200

    Add build version

--- guacamole_image/build.sh
+++ guacamole_image/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker build --build-arg JAR_VERSION=$jar_version -t qualix_guacamole .
+S3_BUCKET_URL=https://quali-dev-binaries.s3.amazonaws.com
+VERSION=3.2.0
+if [ $jar_version ]; then
+    VERSION=$jar_version
+fi
+BUILD_VERSION=$(curl -s $S3_BUCKET_URL/?prefix=qualix-$VERSION|grep -o "qualix-$VERSION\.*[0-9]*.tar.gz"|sort -V|tail -n 1|grep -o "$VERSION\.\+[0-9]\+"||echo $VERSION)
+echo Build $BUILD_VERSION
+docker build --build-arg JAR_VERSION=$VERSION --build-arg BUILD_VERSION=$BUILD_VERSION -t qualix_guacamole .
 
